da546c560e4e342592c631467ed1666b46395fa8
benluks <70067243+benluks@users.noreply.github.com>
2022-05-09 15:41:07 +0200

    fix mistake in debug print

--- bin/train_asr.py
+++ bin/train_asr.py
@@ -270,7 +270,7 @@ class Solver(BaseSolver):
             if i == len(self.dv_set)//2:
                 print("made it here")
                 print(self.tokenizer.decode(
-                            att_output[i].argmax(dim=-1).tolist()))
+                            att_output[0].argmax(dim=-1).tolist()))
                 exit()
                 print(self.tokenizer.decode(
                             att_output[0].argmax(dim=-1).tolist()))
