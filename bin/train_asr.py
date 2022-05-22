ee968bb5e53a7349de9d48c21ee58df5be733184
benluks <70067243+benluks@users.noreply.github.com>
2022-05-22 14:41:16 +0200

    get rid of print statements; problem was in config file

--- bin/train_asr.py
+++ bin/train_asr.py
@@ -21,7 +21,7 @@ class Solver(BaseSolver):
         self.best_wer = {'att': 3.0, 'ctc': 3.0}
 
         # enable early stopping
-        self.early_stopping = bool(self.config['hparas']['early_stopping'])
+        self.early_stopping = self.config['hparas']['early_stopping']
         if self.early_stopping:
             self.patience = self.config['hparas']['patience']
             self.es_wait = self.config['hparas']['es_wait']
